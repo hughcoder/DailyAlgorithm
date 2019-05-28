2803b3301d54f65c2b27b8ada63bf5397752846c
chenyw <chenyw@tuya.com>
2019-05-28 09:52:59 +0800

    add

--- src/main/threads/同步相关/AccountingSyncBad.java
+++ src/main/threads/同步相关/AccountingSyncBad.java
@@ -1,4 +1,29 @@
 package main.threads.同步相关;
 
-public class AccountingSyncBad {
+public class AccountingSyncBad implements Runnable {
+    static int i = 0;
+
+    public synchronized void increase() {
+        i++;
+    }
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 1000000; j++) {
+            increase();
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        // 如果我们用synchronized 修饰方法，这时候多线程要同步，我们需要确保是同一对象
+        Thread t1 = new Thread(new AccountingSyncBad());
+        Thread t2 = new Thread(new AccountingSyncBad());
+
+        t1.start();
+        t2.start();
+        //join含义:当前线程A等待thread线程终止之后才能从thread.join()返回
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
 }
---
+++ src/main/threads/同步相关/AccountingSyncClass.java
@@ -0,0 +1,48 @@
+package main.threads.同步相关;
+
+public class AccountingSyncClass implements Runnable {
+
+    static int i =0;
+
+    /**
+     * 作用于静态方法,锁是当前class对象,也就是
+     * AccountingSyncClass类对应的class对象
+     */
+    public static synchronized void increase(){
+        i++;
+    }
+    /**
+     * 非静态,访问时锁不一样不会发生互斥
+     */
+    public synchronized void increase4Obj(){
+        i++;
+    }
+
+
+    @Override
+    public void run() {
+        for(int j=0;j<1000000;j++){
+//            increase();
+            increase4Obj();
+        }
+    }
+
+   // 由于synchronized关键字修饰的是静态increase方法，与修饰实例方法不同的是，其锁对象是当前类的class对象。
+    // 注意代码中的increase4Obj方法是实例方法，其对象锁是当前实例对象，如果别的线程调用该方法，将不会产生互斥现象，
+    // 毕竟锁对象不同，但我们应该意识到这种情况下可能会发现线程安全问题(操作了共享静态变量i)。
+
+    //静态类当中所有实例的属性方法共享
+
+    public static void main(String[] args) throws InterruptedException {
+        //new新实例
+        Thread t1=new Thread(new AccountingSyncClass());
+        //new心事了
+        Thread t2=new Thread(new AccountingSyncClass());
+
+        //启动线程
+        t1.start();t2.start();
+
+        t1.join();t2.join();
+        System.out.println(i);
+    }
+}
