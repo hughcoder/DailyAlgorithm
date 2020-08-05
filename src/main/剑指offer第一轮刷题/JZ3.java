f68b7098d301d6a3f69a375ea82ddd931d96bced
chenyouwei <chenyouwei@qupeiyin.com>
2020-08-05 20:34:16 +0800

    feat:新增一轮题

---
+++ src/main/剑指offer第一轮刷题/JZ3.java
@@ -0,0 +1,34 @@
+package main.剑指offer第一轮刷题;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+public class JZ3 {
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> array = new ArrayList<>();
+        if (listNode == null) {
+            return array;
+        }
+
+        while (listNode != null) {
+            array.add(listNode.val);
+            listNode = listNode.next;
+        }
+        ArrayList<Integer> array2 = new ArrayList<>();
+        for (int i = array.size() - 1; i >= 0; i--) {
+            array2.add(array.get(i));
+        }
+
+        return array2;
+    }
+
+    public static void main(String args[]) {
+        ListNode node =new ListNode(1);
+        node.next = new ListNode(2);
+        System.out.println(printListFromTailToHead(node));
+    }
+}
