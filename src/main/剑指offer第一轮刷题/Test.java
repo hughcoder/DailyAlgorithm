493b549e2056e8ae66db912c1e4e6d81250273f1
chenyouwei <chenyouwei@qupeiyin.com>
2020-08-18 23:36:20 +0800

    feat:新增题型

--- src/main/剑指offer第一轮刷题/Test.java
+++ src/main/剑指offer第一轮刷题/Test.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String args[]) {
-        System.out.println('1'-0);
-        System.out.println('2'-0);
-        System.out.println('3'-0);
-        System.out.println('a'-0);
+        String str = "[1,2,3,null,null,4,5]";
+        String newStr = str.substring(1,str.length()-1);
+        String arr[] = newStr.split(",");
+        System.out.println(arr);
     }
 
 }
