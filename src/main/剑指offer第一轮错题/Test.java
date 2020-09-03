06b9ef523153edfa0ac7667dc8373d9de4f84371
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-03 21:45:40 +0800

    feat:难题订正

--- src/main/剑指offer第一轮错题/Test.java
+++ src/main/剑指offer第一轮错题/Test.java
@@ -2,9 +2,15 @@ package main.剑指offer第一轮错题;
 
 public class Test {
     public static void main(String args[]) {
-        System.out.println(addSum(15));
-        System.out.println(addSum(8));
-        System.out.println(addSum(100));
+//        System.out.println(addSum(15));
+//        System.out.println(addSum(8));
+//        System.out.println(addSum(100));
+//
+//        System.out.println('1');
+//        String str = "1";
+//        System.out.println(str.charAt(0));
+        System.out.println('e'>'1');
+        System.out.println('e'<'9');
     }
 
     private static int addSum(int num) {
