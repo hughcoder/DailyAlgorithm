80e2f5e8dc8081e4215de2313210dd9a33465cdf
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-03 22:45:55 +0800

    feat:订正52

--- src/main/剑指offer第一轮错题/Test.java
+++ src/main/剑指offer第一轮错题/Test.java
@@ -9,8 +9,24 @@ public class Test {
 //        System.out.println('1');
 //        String str = "1";
 //        System.out.println(str.charAt(0));
-        System.out.println('e'>'1');
-        System.out.println('e'<'9');
+        System.out.println('e' > '1');
+        System.out.println('e' < '9');
+        int a = 5;
+        System.out.println(a |= 4);
+
+        boolean flag = true;
+        flag = true;
+        flag |= true;
+        System.out.println("true\t|=\ttrue\t==>\t" + flag);
+        flag = true;
+        flag |= false;
+        System.out.println("true\t|=\tfalse\t==>\t" + flag);
+        flag = false;
+        flag |= true;
+        System.out.println("false\t|=\ttrue\t==>\t" + flag);
+        flag = false;
+        flag |= false;
+        System.out.println("false\t|=\tfalse\t==>\t" + flag + "\n");
     }
 
     private static int addSum(int num) {
