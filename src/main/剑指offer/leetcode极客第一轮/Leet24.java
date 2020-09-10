323fceaea79e920809a43399191f71fea050f827
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-10 13:14:48 +0800

    feat:开始刷极客推荐题目

---
+++ src/main/剑指offer/leetcode极客第一轮/Leet24.java
@@ -0,0 +1,8 @@
+package main.剑指offer.leetcode极客第一轮;
+
+public class Leet24 {
+//    public ListNode swapPairs(ListNode head) {
+//
+//
+//    }
+}
---
+++ src/main/剑指offer/leetcode极客第一轮/TreeNode.java
@@ -0,0 +1,11 @@
+package main.剑指offer.leetcode极客第一轮;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
