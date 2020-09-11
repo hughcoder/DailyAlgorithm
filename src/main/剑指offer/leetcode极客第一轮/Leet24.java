0791c40f914ea284acf19fd2e7a147b5ac0391b9
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-11 13:57:44 +0800

    feat:提交leet1

--- src/main/剑指offer/leetcode极客第一轮/Leet24.java
+++ src/main/剑指offer/leetcode极客第一轮/Leet24.java
@@ -1,8 +1,20 @@
 package main.剑指offer.leetcode极客第一轮;
 
 public class Leet24 {
-//    public ListNode swapPairs(ListNode head) {
-//
-//
-//    }
+
+    public ListNode swapPairs(ListNode head) {
+        //非递归
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while (temp.next != null && temp.next.next!=null) {
+            //temp没轮都在移动
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next =start;
+        }
+        return temp.next;
+    }
 }
