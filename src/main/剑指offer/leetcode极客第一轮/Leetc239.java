1d717389b40d7c3789319074c29cf2b7a5b08465
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-13 10:17:44 +0800

    feat:新增两道队列题

--- src/main/剑指offer/leetcode极客第一轮/Leetc239.java
+++ src/main/剑指offer/leetcode极客第一轮/Leetc239.java
@@ -1,4 +1,32 @@
 package main.剑指offer.leetcode极客第一轮;
 
+import java.util.ArrayList;
+
+//239. 滑动窗口最大值  不是最优解
 public class Leetc239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int maxValue = -Integer.MAX_VALUE;
+            if (k + i <= nums.length) {
+                for (int j = i; j < k + i; j++) {
+                    if (nums[j] > maxValue) {
+                        maxValue = nums[j];
+                    }
+                }
+                list.add(maxValue);
+            }
+        }
+        int[] max = new int[list.size()];
+        for (int a = 0; a < list.size(); a++) {
+            max[a] = list.get(a);
+        }
+        return max;
+    }
+
+    public static void main(String args[]) {
+        Leetc239 stack = new Leetc239();
+        int[] a = {1, 3, -1, -3, 5, 3, 6, 7};
+        stack.maxSlidingWindow(a, 3);
+    }
 }
--- src/main/剑指offer/leetcode极客第一轮/Leetc703.java
+++ src/main/剑指offer/leetcode极客第一轮/Leetc703.java
@@ -1,13 +1,28 @@
 package main.剑指offer.leetcode极客第一轮;
 
+import java.util.*;
+
 //703. 数据流中的第K大元素
 public class Leetc703 {
+    private PriorityQueue<Integer> queue; //默认排序
+
+    int mk = 0;
 
     public Leetc703(int k, int[] nums) {
-
+        mk = k;
+        queue = new PriorityQueue<>();
+        for (int num : nums) {
+            add(num);
+        }
     }
 
     public int add(int val) {
-        return 0;
+        if (queue.size() < mk) {
+            queue.add(val);
+        } else if (val > queue.peek()) {
+            queue.poll();
+            queue.add(val);
+        }
+        return queue.peek();
     }
 }
