babc3f273b2510048eaac5edc8c8456465661ce5
hughcoder <849978368@qq.com>
2020-10-22 13:15:43 +0800

    feat:新增题目

--- src/main/剑指offer/leetcode极客第二轮/Leetm120.java
+++ src/main/剑指offer/leetcode极客第二轮/Leetm120.java
@@ -3,7 +3,25 @@ package main.剑指offer.leetcode极客第二轮;
 import java.util.List;
 
 public class Leetm120 {
+    //一把过了 虽然感觉还是很不熟
     public int minimumTotal(List<List<Integer>> triangle) {
-        return 0;
+        int length = triangle.size();
+        int[][] dp = new int[length][length];
+        //自底向上  不需要考虑边界值问题
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                if (i == length - 1) {
+                    //初始化
+                    dp[i][j] = triangle.get(i).get(j);
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+                }
+            }
+        }
+
+        return dp[0][0];
+
     }
+
+
 }
--- src/main/剑指offer/leetcode极客第二轮/Leetm152.java
+++ src/main/剑指offer/leetcode极客第二轮/Leetm152.java
@@ -2,9 +2,7 @@ package main.剑指offer.leetcode极客第二轮;
 
 public class Leetm152 {
     //给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
-    //令imax为当前最大值，则当前最大值为 imax = max(imax * nums[i], nums[i])
-    //由于存在负数，那么会导致最大的变最小的，最小的变最大的。因此还需要维护当前最小值imin，imin = min(imin * nums[i], nums[i])
-
+    //存在负数 意味着还需要在维护最大值的同时还要维护最小值
     public int maxProduct(int[] nums) {
       return 0;
     }
