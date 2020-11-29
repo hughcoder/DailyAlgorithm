fc7e2b2745efa890dcfed59a4405746e96546068
hughcoder <849978368@qq.com>
2020-11-29 23:12:11 +0800

    feat:新增题目

--- src/main/剑指offer/第三阶段刷题/JZ24.java
+++ src/main/剑指offer/第三阶段刷题/JZ24.java
@@ -1,15 +1,28 @@
 package main.剑指offer.第三阶段刷题;
 
+import java.util.Stack;
+
 public class JZ24 {
     //定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
     //输入: 1->2->3->4->5->NULL
     //输出: 5->4->3->2->1->NULL
-//    public ListNode reverseList(ListNode head) {
-//        ListNode node =  new ListNode(0);
-//        ListNode temp = null;
-//        while (head!=null){
-//
-//
-//        }
-//    }
+    public ListNode reverseList2(ListNode head) {
+        if(head == null){
+            return null;
+        }
+       //用栈来实现
+        Stack<Integer> stack = new Stack<>();
+
+        while (head!=null){
+            stack.push(head.val);
+            head = head.next;
+        }
+        ListNode node = new ListNode(stack.pop());
+        ListNode temp = node;
+        while (!stack.isEmpty()){
+            node.next = new ListNode(stack.pop());
+            node = node.next;
+        }
+        return temp;
+    }
 }
--- src/main/剑指offer/第三阶段刷题/JZ26.java
+++ src/main/剑指offer/第三阶段刷题/JZ26.java
@@ -1,4 +1,8 @@
 package main.剑指offer.第三阶段刷题;
 
 public class JZ26 {
+    //若树B是树A的子结构
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+
+    }
 }
