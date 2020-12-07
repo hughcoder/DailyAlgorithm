9e1324327384b8ae09246300146e3b67328273c7
hughcoder <849978368@qq.com>
2020-12-07 18:13:43 +0800

    feat:新增题型

--- src/main/剑指offer/第三阶段刷题/JZ30.java
+++ src/main/剑指offer/第三阶段刷题/JZ30.java
@@ -1,4 +1,28 @@
 package main.剑指offer.第三阶段刷题;
 
 public class JZ30 {
+    /** initialize your data structure here. */
+    public JZ30() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+    return 0;
+    }
+
+    public int min() {
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        JZ30 jz30 = new JZ30();
+    }
 }
