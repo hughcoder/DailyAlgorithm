e1c73b273b1da796828b07278ed3b63929e0c834
hughcoder <849978368@qq.com>
2020-12-15 21:39:32 +0800

    feat:修改

--- src/main/剑指offer/第三阶段刷题/JZ34.java
+++ src/main/剑指offer/第三阶段刷题/JZ34.java
@@ -1,4 +1,40 @@
 package main.剑指offer.第三阶段刷题;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JZ34 {
+    List<List<Integer>> allPath;
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        allPath = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        recur(root, tempList, sum);
+        return allPath;
+    }
+
+    public void recur(TreeNode root, List<Integer> list, int sum) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        if (getAll(list) == sum) {
+            allPath.add(new ArrayList<>(list));
+        }
+        recur(root.left, list, sum);
+        recur(root.right, list, sum);
+        list.remove(list.size() - 1);
+    }
+
+    private int getAll(List<Integer> list) {
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return sum;
+
+    }
 }
