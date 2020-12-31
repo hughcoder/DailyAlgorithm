e9dd1f26b271b920a1c242dc3d78d1c2d85e38ba
hughcoder <849978368@qq.com>
2020-12-31 10:11:37 +0800

    feat:新增题目

--- src/main/剑指offer/第三阶段刷题/JZ42.java
+++ src/main/剑指offer/第三阶段刷题/JZ42.java
@@ -3,6 +3,23 @@ package main.剑指offer.第三阶段刷题;
 public class JZ42 {
     //连续子数组的最大和
     public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int max = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
 
+        return max;
+    }
+
+    public static void main(String args[]) {
+        JZ42 jz42 = new JZ42();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(jz42.maxSubArray(nums));
     }
 }
--- src/main/剑指offer/第三阶段刷题/JZ43.java
+++ src/main/剑指offer/第三阶段刷题/JZ43.java
@@ -1,4 +1,27 @@
 package main.剑指offer.第三阶段刷题;
 
 public class JZ43 {
+    //输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+    //
+    //例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+
+    public int countDigitOne(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i =0;i<=n;i++){
+            sb.append(i);
+        }
+        int count = 0;
+        String str = sb.toString();
+        for(int i =0;i<str.length();i++){
+            if(str.charAt(i) == '1'){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String args[]) {
+        JZ43 jz43 = new JZ43();
+        System.out.println(jz43.countDigitOne(12));
+    }
 }
