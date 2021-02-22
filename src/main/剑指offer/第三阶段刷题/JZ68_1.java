49ce0f0908fb931fef7fa776256eb3754112cdda
hughcoder <849978368@qq.com>
2021-02-22 18:54:43 +0800

    feat:新增题目

--- src/main/剑指offer/第三阶段刷题/JZ68_1.java
+++ src/main/剑指offer/第三阶段刷题/JZ68_1.java
@@ -1,8 +1,21 @@
 package main.剑指offer.第三阶段刷题;
 
 public class JZ68_1 {
+    //后续遍历
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       
+        if (root == null ) {
+            return root;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+       TreeNode left = lowestCommonAncestor(root.left,p,q);
+       TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        return left == null ? right : right == null ? left : root;
+
     }
 
     public static void main(String args[]) {
--- src/main/剑指offer/第三阶段刷题/JZ68_2.java
+++ src/main/剑指offer/第三阶段刷题/JZ68_2.java
@@ -1,4 +1,18 @@
 package main.剑指offer.第三阶段刷题;
 
 public class JZ68_2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null ) {
+            return root;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        return left == null ? right : right == null ? left : root;
+    }
 }
